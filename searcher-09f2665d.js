b5ccd2cb587883542c438c63fc57e9205196e2c9
Cybozu Neco <cybozu-neco@users.noreply.github.com>
2026-07-02 01:52:17 +0000

    update

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-81b710b8.js',
+                path_to_root + 'searchindex-93b6973f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
